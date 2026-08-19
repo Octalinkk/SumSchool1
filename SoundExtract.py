5e0583a1034f2e12a2165f92a6415f14ec0dcffd
Octalink <btfoctalink@gmail.com>
2026-08-19 09:27:16 +0200

    Typping return func

--- SoundExtract.py
+++ SoundExtract.py
@@ -1,13 +1,14 @@
 
 import pretty_midi
-
+from pretty_midi import Note
+import typing
 
 class SoundExtract:
 
     def __init__(self, path:str):
         self.midi_data = pretty_midi.PrettyMIDI(path) 
 
-    def getNotesForIntru(self, IntruId:int=0):
+    def getNotesForIntru(self, IntruId:int=0) -> typing.List[Note]:
         if (IntruId > len(self.midi_data.instruments)-1) : IntruId = len(self.midi_data.instruments)-1
         return self.midi_data.instruments[IntruId].notes
         # Rassembler toutes les notes de tous les instruments dans une seule liste triée par temps de début
