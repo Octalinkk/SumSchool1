fcf1caf9c357de1dec17a9b768a5599234215296
Octalink <btfoctalink@gmail.com>
2026-08-20 15:19:19 +0200

    Gen Ship Separated

--- Spaceship.py
+++ Spaceship.py
@@ -1,19 +1,22 @@
 from pygame import Vector2, draw
 import math
+from Triangle import Triangle
 
 class SpaceShip():
     def __init__(self, origin:Vector2, seed):
         self.angle = 0
         self.origin = origin
-        self.headPoints = []
-        self.bodyPoints = []
-        self.wingsPoints = []
-        self.propPoints = []
-        self.boostPoints = []
+        self.headPoints:list[Vector2] = []
+        self.bodyPoints:list[Vector2] = []
+        self.wingsPoints:list[Vector2] = []
+        self.propPoints:list[Vector2] = []
+        self.boostPoints:list[Vector2] = []
 
           
-        self.sideLen = 70
-        self.angleBody = self.degToRad(15)
+        self.sideLen:int = 70
+        self.angleBody:float = self.degToRad(15)
+
+        self.genShip()
 
     def degToRad(self, angle):
         return angle * math.pi / 180
@@ -22,14 +25,17 @@ class SpaceShip():
 
         positionX = pos.x
         positionY = pos.y
-        triangles = []
+        triangles:list[Triangle] = []
 
         for i in range(details):
             angle1 = i * (2 * math.pi / details)
             angle2 = (i + 1) * (2 * math.pi / details)
-            triangles.append([(positionX, positionY),
-            (positionX + radius * math.cos(angle1), positionY + radius * math.sin(angle1)),
-            (positionX + radius * math.cos(angle2), positionY + radius * math.sin(angle2))])
+            triangles.append(Triangle(Vector2(positionX, positionY),
+                                            Vector2((positionX + radius * math.cos(angle1), positionY + radius * math.sin(angle1))),
+            Vector2((positionX + radius * math.cos(angle2), positionY + radius * math.sin(angle2)))))
+
+    def rotateShip(self, target:Vector2, angleRad:float):
+        ...
 
     def genShip(self):
         self.genHead1()
@@ -37,22 +43,28 @@ class SpaceShip():
         self.genWings1()
         self.genProp1()
 
-    def drawPart(self, screen, datas):
+    def drawShip(self, screen):
+        self.drawPart(screen, self.headPoints)
+        self.drawPart(screen, self.bodyPoints)
+        self.drawPart(screen, self.wingsPoints)
+        self.drawPart(screen, self.propPoints)
+
+    def drawPart(self, screen, datas:list[Triangle]):
         for triangle in datas:
-            draw.polygon(screen, (255,0,0), triangle)
+            triangle.draw(screen)
 
     def genHead1(self):
         point1 = Vector2(self.origin.x + self.sideLen * math.cos(self.angleBody + self.angle), self.origin.y + self.sideLen * math.sin(self.angleBody + self.angle))
         point2 = Vector2(self.origin.x + self.sideLen * math.cos(-(self.angleBody - self.angle)), self.origin.y + self.sideLen * math.sin(-(self.angleBody - self.angle )))
         #draw.polygon(screen, (255, 255, 0), [self.origin, point1, point2])
-        self.headPoints = [[self.origin, point1, point2]]
+        self.headPoints = [Triangle(self.origin, point1, point2)]
 
     def genBody1(self):      
         point1 = Vector2(self.origin.x + self.sideLen * math.cos(self.angleBody + self.angle), self.origin.y + self.sideLen * math.sin(self.angleBody + self.angle))
         point2 = Vector2(self.origin.x + self.sideLen * math.cos(-(self.angleBody - self.angle)), self.origin.y + self.sideLen * math.sin(-(self.angleBody - self.angle )))
         point3 = Vector2(self.origin.x + (self.sideLen+100) * math.cos(self.angle), self.origin.y + (self.sideLen+100) * math.sin(self.angle))        
         #draw.polygon(screen, (255, 0, 0), [point3, point1, point2])
-        self.bodyPoints = [[point3, point1, point2]]
+        self.bodyPoints = [Triangle(point3, point1, point2)]
 
     def genWings1(self):
         point3Up = Vector2(self.origin.x + (self.sideLen+45) * math.cos(2*self.angle+self.degToRad(5)), self.origin.y + (self.sideLen+45) * math.sin(2*self.angle+self.degToRad(5)))
@@ -64,14 +76,14 @@ class SpaceShip():
         point2Down = Vector2(self.origin.x + (self.sideLen+75) * math.cos(self.angle- (self.angle+self.degToRad(1))), self.origin.y + (self.sideLen+86) * math.sin(self.angle- (self.angle+self.degToRad(1))))
         point1Down = Vector2(self.origin.x + (self.sideLen+150) * math.cos(self.angle- (self.angle+self.degToRad(30))), self.origin.y + (self.sideLen+80) * math.sin(self.angle- (self.angle+self.degToRad(30))))
         #draw.polygon(screen, (0, 255, 0), [point1Down, point2Down, point3Down])
-        self.wingsPoints = [[point1Up, point2Up, point3Up], [point1Down, point2Down, point3Down]]    
+        self.wingsPoints = [Triangle(point1Up, point2Up, point3Up), Triangle(point1Down, point2Down, point3Down)]    
 
     def genProp1(self):
         point1 = Vector2(self.origin.x + (self.sideLen+80) * math.cos(self.angle), self.origin.y + (self.sideLen+80) * math.sin(self.angle)) 
         point2 = Vector2(self.origin.x + (self.sideLen+110) * math.cos(self.angle+self.degToRad(5)), self.origin.y + (self.sideLen+110) * math.sin(self.angle+self.degToRad(5)))
         point3 = Vector2(self.origin.x + (self.sideLen+110) * math.cos(-self.angle-self.degToRad(5)), self.origin.y + (self.sideLen+110) * math.sin(-self.angle-self.degToRad(5)))
         #draw.polygon(screen, (0, 0, 255), [point1, point2, point3]) 
-        self.propPoints = [[point1, point2, point3]]
+        self.propPoints = [Triangle(point1, point2, point3)]
 
     def genBoosters(self):
         point1Up = Vector2(self.origin.x + (self.sideLen+100) * math.cos(2*self.angle+self.degToRad(15)), self.origin.y + (self.sideLen+100) * math.sin(2*self.angle+self.degToRad(15)))
@@ -83,7 +95,7 @@ class SpaceShip():
         point2Down = Vector2(point1Down.x + (20) * math.cos(self.angle+self.degToRad(10)), point1Down.y + (20) * math.sin(self.angle+self.degToRad(10)))
         point3Down = Vector2(point1Down.x + (20) * math.cos(self.angle-self.degToRad(10)), point1Down.y + (20) * math.sin(self.angle-self.degToRad(10)))        
         #draw.polygon(screen, (0, 255, 255), [point1Down, point2Down, point3Down])  
-        self.boostPoints = [[point1Up, point2Up, point3Up], [point1Down, point2Down, point3Down]]  
+        self.boostPoints = [Triangle(point1Up, point2Up, point3Up), Triangle(point1Down, point2Down, point3Down)]  
 
     def genHead2(self):
         radius = 30
@@ -97,7 +109,7 @@ class SpaceShip():
 
         #draw.polygon(screen, (255, 255, 0), [point1, point2, point3])         
         #draw.polygon(screen, (255, 255, 0), [point3, point2, point4]) 
-        self.headPoints = [circle, [point1, point2, point3], [point3, point2, point4]]
+        self.headPoints = [circle, Triangle(point1, point2, point3), Triangle(point3, point2, point4)]
 
 
     def genBody2(self):
@@ -109,7 +121,7 @@ class SpaceShip():
         point4 = Vector2(border.x + (35) * math.cos(self.angle + self.degToRad(-90)), border.y + (35) * math.sin(self.angle + self.degToRad(-90)))          
         #draw.polygon(screen, (255, 0, 0), [point1, point2, point3])         
         #draw.polygon(screen, (255, 0, 0), [point3, point2, point4]) 
-        self.bodyPoints = [[point1, point2, point3], [point3, point2, point4]]    
+        self.bodyPoints = [Triangle(point1, point2, point3), Triangle(point3, point2, point4)]    
 
     def genWings2(self):        
         width = 35
@@ -125,7 +137,7 @@ class SpaceShip():
         point4Down = Vector2(point3Down.x + (2*width) * math.cos(self.angle + self.degToRad(0)), point3Down.y + (2*width) * math.sin(self.angle + self.degToRad(0)))      
         #draw.polygon(screen, (0, 255, 0), [point1, point2, point3Down])         
         #draw.polygon(screen, (0, 255, 0), [point3Down, point1, point4Down]) 
-        self.wingsPoints = [[point1, point2, point3Up], [point3Up, point1, point4Up], [point1, point2, point3Down], [point3Down, point1, point4Down])]   
+        self.wingsPoints = [Triangle(point1, point2, point3Up), Triangle(point3Up, point1, point4Up), Triangle(point1, point2, point3Down), Triangle(point3Down, point1, point4Down)]   
 
     def genProp2(self):     
         width = 15
@@ -139,4 +151,4 @@ class SpaceShip():
         #draw.polygon(screen, (0, 0, 255), [anchor, point3, point4])
         #draw.polygon(screen, (0, 0, 255), [anchor, point1, point3])         
         #draw.polygon(screen, (0, 0, 255), [anchor, point2, point4])
-        self.propPoints = [[anchor, point3, point4], [anchor, point1, point3], [anchor, point2, point4]]
+        self.propPoints = [Triangle(anchor, point3, point4), Triangle(anchor, point1, point3), Triangle(anchor, point2, point4)]
